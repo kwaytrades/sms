3bbc0370fd7639cb0c8bfb9b1e01371f7a1512ef
kwaytrades <kcoffie90@gmail.com>
2025-07-25 14:01:26 +0000

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+from .user import UserProfile, PlanLimits
+from .conversation import ChatMessage, Conversation
+from .trading import TradingData, Portfolio
+
+__all__ = [
+    "UserProfile", 
+    "PlanLimits", 
+    "ChatMessage", 
+    "Conversation", 
+    "TradingData", 
+    "Portfolio"
+]
