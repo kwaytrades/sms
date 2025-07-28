063db0b5c65d1fb25661bdc0724993a6a684d8ce
ktrades <kcoffie90@gmail.com>
2025-07-28 20:46:03 +0000

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -3,6 +3,8 @@ from .openai_service import OpenAIService
 from .technical_analysis import TechnicalAnalysisService
 from .twilio_service import TwilioService
 from .llm_agent import TradingAgent, ToolExecutor
+from .news_sentiment import NewsSentimentService  # ← ADD THIS
+from .fundamental_analysis import FundamentalAnalysisEngine, FundamentalAnalysisTool  # ← ADD THIS
 
 __all__ = [
     "DatabaseService",
@@ -10,5 +12,8 @@ __all__ = [
     "TechnicalAnalysisService", 
     "TwilioService",
     "TradingAgent",
-    "ToolExecutor"
+    "ToolExecutor",
+    "NewsSentimentService",  # ← ADD THIS
+    "FundamentalAnalysisEngine",  # ← ADD THIS
+    "FundamentalAnalysisTool"  # ← ADD THIS
 ]
