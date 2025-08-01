3eac603889ac4a1445802fa69f280d05e0360515
ktrades <kcoffie90@gmail.com>
2025-08-01 23:45:48 +0000

    Create __init__.py

---
+++ social/services/platform_services/__init__.py
@@ -0,0 +1,10 @@
+# social/services/platform_services/__init__.py
+from .tiktok_service import TikTokService
+
+# Future platform imports
+# from .twitter_service import TwitterService
+# from .instagram_service import InstagramService
+
+__all__ = [
+    "TikTokService"
+]
